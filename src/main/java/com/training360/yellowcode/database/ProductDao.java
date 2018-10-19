22c3d54e76fcf4b1183179e3d18b0a428e8471a8
Zoltán Karácsonyi <zoltankaracsonyi91@gmail.com>
2018-10-19 14:35:05 +0200

    ProductDao init

---
+++ src/main/java/com/training360/yellowcode/database/ProductDao.java
@@ -0,0 +1,35 @@
+package com.training360.yellowcode.database;
+
+import com.training360.yellowcode.dbTables.Product;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class ProductDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public ProductDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private static class ProductMapper implements RowMapper<Product> {
+        @Override
+        public Optional<Product> mapRow(ResultSet resultSet, int i) throws SQLException {
+            long id = resultSet.getLong("id");
+            String name = resultSet.getString("name");
+            String address = resultSet.getString("address");
+            String producer = resultSet.getString("producer");
+            Long currentPrice = resultSet.getLong("price");
+            Product product = new Product(id,name,address,producer,currentPrice);
+            return null;
+        }
+    }
+
+    public Optional<Product> findProductByAddress(String address){
+        return jdbcTemplate.queryForObject("select id, name from employees where id = ?",new ProductMapper(),address);
+    }
+}
