68f782395c1eac43d59d7102a909fe97928f27b6
Hajdú Mózes <hajdu.mozes@gmail.com>
2018-10-19 13:18:36 +0200

    project init

---
+++ src/main/java/com/training360/yellowcode/YellowcodeApplication.java
@@ -0,0 +1,12 @@
+package com.training360.yellowcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowcodeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/training360/yellowcode/YellowcodeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.training360.yellowcode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YellowcodeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
