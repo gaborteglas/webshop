30a6b106731e6f99f6809f296414e5243eb2716d
Zoltán Karácsonyi <zoltankaracsonyi91@gmail.com>
2018-10-19 20:31:00 +0200

    product site done

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -3,7 +3,7 @@ window.onload = function() {
     }
 
 function updateTable() {
- fetch("api/products/A Java ura: A classok szövetsége")
+ fetch("api/products/szovetseg")
      .then(function(request) {
          return request.json();
      })
@@ -13,7 +13,32 @@ function updateTable() {
  }
 
  function fillTable(jsonData){
+    let name = jsonData.name;
+    let id = jsonData.id;
+    let producer = jsonData.producer;
+    let currentPrice = jsonData.currentPrice + " Ft";
+    creatingHeaderForName(name);
+    creatingTableRowForData(id,producer,currentPrice)
+    console.log(jsonData)
+ }
+
+ function creatingHeaderForName(name){
     let productName = document.querySelector("#product-name");
-    let name = jsonData.name
     productName.innerHTML = name;
+ }
+
+ function creatingTableRowForData(id,producer,currentPrice){
+    let tbody = document.querySelector("#employees-tbody");
+    let tr = document.createElement("tr");
+    tr["raw-data"] = id;
+    let idTd = document.createElement("td");
+    let producerTd = document.createElement("td");
+    let currentPriceTd = document.createElement("td");
+    idTd.innerHTML = id;
+    producerTd.innerHTML = producer;
+    currentPriceTd.innerHTML = currentPrice;
+    tr.appendChild(idTd);
+    tr.appendChild(producerTd);
+    tr.appendChild(currentPriceTd);
+    tbody.appendChild(tr);
  }
