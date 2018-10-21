b9d6c46f60cc106a166487fff735de16087cf7e1
Zoltán Karácsonyi <zoltankaracsonyi91@gmail.com>
2018-10-21 14:22:29 +0200

    db create basket.sql

--- src/main/resources/db/migration/V3__basket.sql
+++ src/main/resources/db/migration/V3__basket.sql
@@ -2,8 +2,8 @@ CREATE TABLE basket (
 id BIGINT auto_increment,
 user_name varchar(255) NOT NULL,
 product_id bigint NOT NULL,
-quantity BIGINT NOT NULL
+quantity BIGINT NOT NULL,
 CONSTRAINT pk_basket PRIMARY KEY(id),
-CONSTRAINT fk_products FOREIGN KEY(products.id),
-CONSTRAINT fk_users FOREIGN KEY(users.user_name)
+FOREIGN KEY (product_id) REFERENCES products(id),
+FOREIGN KEY (user_name) REFERENCES users(user_name)
 );
