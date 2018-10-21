a681db4ec5a9d8d35f89d4735f0ad1c5a9ead7f1
Téglás Gábor <gaborteglas@gmail.com>
2018-10-21 13:14:54 +0200

    User Class, UserController Class added, SpringSecurity changed

---
+++ src/main/java/com/training360/yellowcode/dbTables/User.java
@@ -0,0 +1,58 @@
+package com.training360.yellowcode.dbTables;
+
+public class User {
+
+    private long id;
+    private String loginName;
+    private String fullName;
+    private String password;
+    private UserRole role;
+
+    public User(long id, String loginName, String fullName, String password, UserRole role) {
+        this.id = id;
+        this.loginName = loginName;
+        this.fullName = fullName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(String loginName, String role) {
+        this.loginName = loginName;
+        this.role = UserRole.valueOf(role);
+    }
+
+    public User() {
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role.name();
+    }
+
+    public void setRole(String role) {
+        this.role = UserRole.valueOf(role);
+    }
+}
--- src/main/resources/db/migration/V2__users.sql
+++ src/main/resources/db/migration/V2__users.sql
@@ -3,11 +3,12 @@ CREATE TABLE users (
  user_name VARCHAR(255) NOT NULL UNIQUE,
  full_name VARCHAR(255) NOT NULL,
  password VARCHAR(255) NOT NULL,
- status VARCHAR(50) NOT NULL,
+ enabled int NOT NULL,
+ role VARCHAR(50) NOT NULL,
  CONSTRAINT pk_users PRIMARY KEY(id)
  );
 
  INSERT INTO users
-(user_name, full_name, password, status)
-VALUES("testuser", "Test User", "123", "CUSTOMER"),
-("testadmin", "Test Admin", "456", "ADMIN");
+(user_name, full_name, password, enabled, role)
+VALUES("testuser", "Test User", "$2a$10$qQxb4Kd6JxefHd0ThHcMz.bmv2ndCBd0yJaHJe4cs3XDLbYm3fSRW", 1, "ROLE_CUSTOMER"),
+("testadmin", "Test Admin", "$2a$10$nZGfEnGSgOAb5GDhNDngreSmN3BEC2VUzWnLNwV9GvPUXbNsOukBW", 1, "ROLE_ADMIN");
