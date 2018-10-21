6116e56ad2c6938f1abb248eafa1cd649b2b5111
Dóra Birgés <birgesdora@gmail.com>
2018-10-21 20:47:01 +0200

    added clear.sql to YellowcodeApplicationTests

--- src/test/java/com/training360/yellowcode/YellowcodeApplicationTests.java
+++ src/test/java/com/training360/yellowcode/YellowcodeApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Sql(scripts = "classpath:/clear.sql")
 public class YellowcodeApplicationTests {
 
     @Autowired
