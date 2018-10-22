852e144e5eb46914d383d7f8ada85a1993a56788
Téglás Gábor <gaborteglas@gmail.com>
2018-10-22 18:32:59 +0200

    Show menu, hide menu, show usarname added to menubar.js

---
+++ src/main/resources/static/js/menubar.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', setUserName);
+
+function setUserName() {
+     fetch("api/user")
+             .then(function (request) {
+                 return request.json();
+                 console.log(request.json);
+             })
+             .then(function(jsonData) {
+                 showUser(jsonData);
+                 if (jsonData.role == "ROLE_ADMIN") {
+                 showMenus();
+                 }
+                 if (jsonData.role == "ROLE_USER") {
+                 hideMenus();
+                 }
+             })
+             .catch(error => showUserNotLogged("Vendég"), hideMenus());
+}
+
+function showUser(jsonData) {
+let userDiv = document.getElementById("username");
+userDiv.innerHTML = jsonData.loginName
+}
+
+function showUserNotLogged(name) {
+let userDiv = document.getElementById("username");
+userDiv.innerHTML = name
+}
+
+function hideMenus() {
+let menuToHide1 = document.getElementById("to-hide-product");
+menuToHide1.style.display = "none";
+let menuToHide2 = document.getElementById("to-hide-user");
+menuToHide2.style.display = "none";
+}
+
+function showMenus() {
+let menuToHide1 = document.getElementById("to-hide-product");
+menuToHide1.style.display = "block";
+let menuToHide2 = document.getElementById("to-hide-user");
+menuToHide2.style.display = "block";
+}
