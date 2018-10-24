4874826f94a9987ae141226b25a59c51994b5c4d
Dóra Birgés <birgesdora@gmail.com>
2018-10-24 11:52:35 +0200

    create handleSubmit and handleReset in register.js

---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,51 @@
+window.onload = function() {
+    let registerForm = document.getElementById("register-form");
+    productForm.onsubmit = handleSubmit;
+    productForm.onreset = handleReset;
+}
+
+function handleSubmit() {
+    let lastNameInput = document.getElementById("last-name");
+    let firstNameInput = document.getElementById("first-name")
+    let usernameInput = document.getElementById("username");
+    let passwordInput = document.getElementById("password");
+    let passwordRepeatInput = document.getElementById("password-repeat");
+
+    let fullName = lastNameInput.value + " " + firstNameInput.value;
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    let passwordRepeat = passwordRepeatInput.value
+
+    if (password !== passwordRepeat) {
+        alert("A két jelszó nem egyezik meg.")
+    }
+
+    let user = {
+                "loginName": username,
+                "fullName": fullName,
+                "password": password
+                }
+
+    let url = "api/users";
+
+    fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8"
+                    },
+            body: JSON.stringify(user)
+    }).then(function(response) {
+        handleReset();
+    }
+    });
+
+}
+
+function handleReset() {
+    let usernameInput = document.getElementById("username");
+    usernameInput.value = "";
+    let passwordInput = document.getElementById("password");
+    passwordInput.value = "";
+    let passwordRepeatInput = document.getElementById("password-repeat");
+    passwordRepeatInput.value = "";
+}
