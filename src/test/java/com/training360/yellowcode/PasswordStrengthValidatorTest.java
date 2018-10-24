1540264010ef2cceb8c15762258b12ce4dcdac25
Hajdú Mózes <hajdu.mozes@gmail.com>
2018-10-24 14:55:03 +0200

    PasswordValidator class created

--- src/test/java/com/training360/yellowcode/PasswordStrengthValidatorTest.java
+++ src/test/java/com/training360/yellowcode/PasswordStrengthValidatorTest.java
@@ -1,45 +1,35 @@
 package com.training360.yellowcode;
 
-import com.training360.yellowcode.userinterface.UserController;
-import junit.framework.Assert;
+import com.training360.yellowcode.businesslogic.PasswordValidator;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
-@SpringBootTest
-@RunWith(SpringRunner.class)
 public class PasswordStrengthValidatorTest {
-    @Autowired
-    private UserController userController;
 
     @Test
     public void passwordStrengthValidatorTestValid() {
-        assertTrue(userController.passwordStrengthValidator("JohnDoe550$"));
+        assertTrue(new PasswordValidator().passwordStrengthValidator("JohnDoe550$"));
     }
 
     @Test
     public void passwordStrengthValidatorTestSufferingLevel() {
-        assertTrue(userController.passwordStrengthValidator("@UO80pJmWsN&U#O^!UCB^i!IOTNO57"));
+        assertTrue(new PasswordValidator().passwordStrengthValidator("@UO80pJmWsN&U#O^!UCB^i!IOTNO57"));
     }
 
     @Test
     public void passwordStrengthValidatorTestWithoutNumber() {
-        assertFalse(userController.passwordStrengthValidator("JohnDoee"));
+        assertFalse(new PasswordValidator().passwordStrengthValidator("JohnDoee"));
     }
 
     @Test
     public void passwordStrengthValidatorTestWithoutCapitalLetter() {
-        assertFalse(userController.passwordStrengthValidator("johndoe220"));
+        assertFalse(new PasswordValidator().passwordStrengthValidator("johndoe220"));
     }
 
     @Test
     public void passwordStrengthValidatorTestTooShort() {
-        assertFalse(userController.passwordStrengthValidator("Valami1"));
+        assertFalse(new PasswordValidator().passwordStrengthValidator("Valami1"));
     }
 }
