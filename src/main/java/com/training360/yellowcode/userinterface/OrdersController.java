8aa2f189ddf466b3a94e063d6fc3ccb4f180a134
Zoltán Karácsonyi <zoltankaracsonyi91@gmail.com>
2018-10-25 12:15:46 +0200

    proper rest api endpoint for orderscontroller

--- src/main/java/com/training360/yellowcode/userinterface/OrdersController.java
+++ src/main/java/com/training360/yellowcode/userinterface/OrdersController.java
@@ -3,16 +3,13 @@ package com.training360.yellowcode.userinterface;
 import com.training360.yellowcode.businesslogic.OrdersService;
 import com.training360.yellowcode.database.DuplicateProductException;
 import com.training360.yellowcode.dbTables.Orders;
-import com.training360.yellowcode.dbTables.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@Controller
+@RestController
 public class OrdersController {
 
     private OrdersService ordersService;
@@ -26,11 +23,11 @@ public class OrdersController {
         return ordersService.listOrders();
     }
 
-    @RequestMapping(value = "/api/orders", method = RequestMethod.POST)
-    public ResponseEntity<String> createOrders(@RequestBody Orders orders) {
+    @RequestMapping(value = "/api/orders/{id}", method = RequestMethod.POST)
+    public ResponseEntity<String> createOrders(@PathVariable long id){
         try {
-            ordersService.createOrders(orders.getUserId());
-            return ResponseEntity.ok("Successfully created.");
+        ordersService.createOrders(id);
+        return ResponseEntity.ok("Successfully created.");
         } catch (DuplicateProductException dpe) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
