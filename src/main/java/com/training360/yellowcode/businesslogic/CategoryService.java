d620bccaf904b033d7be61a35266aa2336136c88
Téglás Gábor <gaborteglas@gmail.com>
2018-10-29 14:54:14 +0100

    CategoryDao CRUD implemented

--- src/main/java/com/training360/yellowcode/businesslogic/CategoryService.java
+++ src/main/java/com/training360/yellowcode/businesslogic/CategoryService.java
@@ -4,6 +4,7 @@ import com.training360.yellowcode.database.CategoryDao;
 import com.training360.yellowcode.dbTables.Category;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,4 +19,24 @@ public class CategoryService {
     public Optional<Category> findCategoryById(long id) {
         return categoryDao.findCategoryById(id);
     }
+
+//    public List<Category> listCategorys() {
+//        return categoryDao.listCategorys();
+//    }
+//
+//    public void createCategory(Category category) {
+//        categoryDao.createCategory(category);
+//    }
+//
+//    public void updateCategory(long id, Category category) {
+//        categoryDao.updateCategory(id, category);
+//    }
+//
+//    public void deleteCategoryUpdateProducts(long id) {
+//        categoryDao.deleteCategoryUpdateProducts(id);
+//    }
+//
+//    public void deleteCategory(long id) {
+//        categoryDao.deleteCategory(id);
+//    }
 }
