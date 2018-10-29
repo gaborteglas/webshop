16b9e9d612172b617e51e0ff1916c0a61173448e
Zoltán Karácsonyi <zoltankaracsonyi91@gmail.com>
2018-10-29 15:03:37 +0100

    reports reportsByDateAndStatus frontend + backend works

--- src/main/resources/static/js/reports.js
+++ src/main/resources/static/js/reports.js
@@ -1,7 +1,23 @@
 window.onload = function() {
    updateTable();
+}
 
-};
+function updateTableByStatus(orders){
+       let statusSelector = document.querySelector("#status-select");
+       switch(statusSelector.value) {
+           case "ACTIVE":
+               fillTableForActive(orders);
+               console.log(orders)
+               break;
+           case "DELIVERED":
+               fillTableForDelivered(orders);
+               console.log(orders)
+               break;
+           case "DELETED":
+               fillTableForDeleted(orders);
+               console.log(orders)
+       }
+    }
 
 function updateTable() {
     fetch("api/reports/orders")
@@ -9,24 +25,67 @@ function updateTable() {
             return response.json();
         })
         .then(function (orders) {
-            fillTable(orders);
-            console.log(orders);
+            console.log(orders)
+            let statusSelector = document.querySelector("#status-select");
+            statusSelector.addEventListener("change", function () { updateTableByStatus(orders)})
         });
 }
 
 function fillTable(orders){
     let tbody = document.querySelector("#orders-tbody");
-    for(i in orders){
-        let tr = document.createElement("tr");
+        for(i in orders){
+            let tr = document.createElement("tr");
+            tbody.appendChild(tr);
+            }
+}
 
+function fillTableForActive(orders){
+    let tbody = document.querySelector("#orders-tbody");
+    tbody.innerHTML = "";
+            for(i in orders){
+                if(orders[i].status == "ACTIVE"){
+                    let tr = document.createElement("tr");
+                    let monthTd = document.createElement("td");
+                    monthTd.innerHTML = orders[i].date;
+                    tr.appendChild(monthTd);
+                    let sumTd = document.createElement("td");
+                    sumTd.innerHTML = orders[i].totalPrice;
+                    tr.appendChild(sumTd);
+                    tbody.appendChild(tr);
+                    }
+            }
+}
 
-        let blankTd = document.createElement("td");
-        blankTd.innerHTML = "";
-        tr.appendChild(blankTd);
+function fillTableForDelivered(orders){
+    let tbody = document.querySelector("#orders-tbody");
+    tbody.innerHTML = "";
+            for(i in orders){
+                if(orders[i].status == "DELIVERED"){
+                    let tr = document.createElement("tr");
+                    let monthTd = document.createElement("td");
+                    monthTd.innerHTML = orders[i].date;
+                    tr.appendChild(monthTd);
+                    let sumTd = document.createElement("td");
+                    sumTd.innerHTML = orders[i].totalPrice;
+                    tr.appendChild(sumTd);
+                    tbody.appendChild(tr);
+                    }
+            }
+}
 
-
-
-
-        tbody.appendChild(tr);
-        }
-    }
+function fillTableForDeleted(orders){
+    let tbody = document.querySelector("#orders-tbody");
+    tbody.innerHTML = "";
+            for(i in orders){
+                if(orders[i].status == "DELETED"){
+                    let tr = document.createElement("tr");
+                    let monthTd = document.createElement("td");
+                    monthTd.innerHTML = orders[i].date;
+                    tr.appendChild(monthTd);
+                    let sumTd = document.createElement("td");
+                    sumTd.innerHTML = orders[i].totalPrice;
+                    tr.appendChild(sumTd);
+                    tbody.appendChild(tr);
+                    }
+            }
+}
