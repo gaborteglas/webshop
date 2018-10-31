e5ee55d0284b2eaf437db94ea66de48542727a05
Dóra Birgés <birgesdora@gmail.com>
2018-10-31 11:02:54 +0100

    create feedback class

---
+++ src/main/java/com/training360/yellowcode/dbTables/Feedback.java
@@ -0,0 +1,51 @@
+package com.training360.yellowcode.dbTables;
+
+import java.time.LocalDateTime;
+
+public class Feedback {
+
+    private long ratingId;
+    private int ratingScore;
+    private String ratingText;
+    private LocalDateTime ratingDate;
+    private User user;
+
+    public Feedback(int ratingScore, String ratingText, LocalDateTime ratingDate, User user) {
+        this.ratingScore = ratingScore;
+        this.ratingText = ratingText;
+        this.ratingDate = ratingDate;
+        this.user = user;
+    }
+
+    public int getRatingScore() {
+        return ratingScore;
+    }
+
+    public void setRatingScore(int ratingScore) {
+        this.ratingScore = ratingScore;
+    }
+
+    public String getRatingText() {
+        return ratingText;
+    }
+
+    public void setRatingText(String ratingText) {
+        this.ratingText = ratingText;
+    }
+
+    public LocalDateTime getRatingDate() {
+        return ratingDate;
+    }
+
+    public void setRatingDate(LocalDateTime ratingDate) {
+        this.ratingDate = ratingDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
