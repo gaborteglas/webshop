c767283cb0409885b4e84c99bb0653554b8b650c
Dóra Birgés <birgesdora@gmail.com>
2018-11-01 19:31:39 +0100

    fix registration

--- src/main/java/com/training360/yellowcode/database/FeedbackDao.java
+++ src/main/java/com/training360/yellowcode/database/FeedbackDao.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public class FeedbackDao {
