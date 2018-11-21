47ff5463061541821068c9a05e6cfc556c677fa5
Hajdú Mózes <hajdu.mozes@gmail.com>
2018-11-21 21:42:49 +0100

    Menubar functions moved to menubar.js

--- src/main/resources/static/js/menubar.js
+++ src/main/resources/static/js/menubar.js
@@ -1,3 +1,9 @@
+window.onload = function () {
+    updateCart();
+    let cartResetButton = document.querySelector("#basket-reset-button");
+    cartResetButton.onclick = handleResetButton;
+}
+
 window.addEventListener('load', setUserName());
 
 function setUserName() {
@@ -115,4 +121,81 @@ let menuToShowIndexUnreg = document.getElementById("to-hide-index");
  menuToShowIndexUnreg.style.display = "block";
  let menuToShowProfileForUser = document.getElementById("to-hide-profile");
  menuToShowProfileForUser.style.display = "none";
+}
+
+function updateCart() {
+    fetch("api/basket")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (products) {
+            fillCart(products);
+        });
+}
+
+function fillCart(products) {
+    let totalPrice = 0
+    let totalQuantity = 0;
+    let cart = document.querySelector(".header-cart-wrapitem");
+    let totalPriceField = document.querySelector(".header-cart-total");
+    let cartQuantity = document.querySelector(".header-icons-noti");
+    for (let k = 0; k < products.length; k++) {
+        let cartElement = document.createElement("li");
+        cartElement.className = "header-cart-item";
+
+        let imageHolderDiv = document.createElement("div");
+        imageHolderDiv.className = "header-cart-item-img";
+
+        let imageOfProduct = document.createElement("img");
+        imageOfProduct.src = "images/item-cart-01.jpg";
+        imageOfProduct.alt = "IMG";
+
+        imageHolderDiv.appendChild(imageOfProduct);
+
+        let textHolderDiv = document.createElement("div");
+        textHolderDiv.className = "header-cart-item-txt";
+
+        let anchor = document.createElement("a");
+        anchor.href = "#";
+        anchor.className = "header-cart-item-name";
+        anchor.innerHTML = products[k].name;
+
+        let price = document.createElement("span");
+        price.className = "header-cart-item-info";
+        price.innerHTML = products[k].quantity + " x " + products[k].currentPrice + " Ft";
+
+        textHolderDiv.appendChild(anchor);
+        textHolderDiv.appendChild(price);
+
+        cartElement.appendChild(imageHolderDiv);
+        cartElement.appendChild(textHolderDiv);
+        cart.appendChild(cartElement);
+
+        totalPrice = totalPrice + (products[k].quantity * products[k].currentPrice);
+        totalQuantity += products[k].quantity;
+    }
+    cartQuantity.innerHTML = totalQuantity;
+    totalPriceField.innerHTML = "Összesen: " + totalPrice + " Ft";
+}
+
+function emptyCartVisually() {
+    let totalPriceField = document.querySelector(".header-cart-total");
+    let cartQuantity = document.querySelector(".header-icons-noti");
+    totalPriceField.innerHTML = "Összesen: 0 Ft";
+    cartQuantity.innerHTML = 0;
+    let cart = document.querySelector(".header-cart-wrapitem");
+    while(cart.hasChildNodes) {
+        cart.removeChild(cart.lastChild);
+    }
+}
+
+function handleResetButton() {
+    if (confirm("Biztos hogy üríteni szeretné a kosár tartalmát?")) {
+        fetch("api/basket/", {
+            method: "DELETE"
+        }).then(function (response) {
+            emptyCartVisually();
+            return response.json()
+        }).then(responseJson => updateTable())
+    }
 }
