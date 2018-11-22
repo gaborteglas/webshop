cb2a7f89273ff369a75328effcb3e6805d18a10f
Hajdú Mózes <hajdu.mozes@gmail.com>
2018-11-22 18:39:47 +0100

    Images shown on products

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -56,8 +56,8 @@ function updateTable(categoryId) {
 function fillTable(products) {
     let productContainer = document.getElementById("product-container");
     //törölni while-lal
-//    let categorySelector = document.querySelector("#category-selector");
-//    let value = categorySelector.value;
+    //    let categorySelector = document.querySelector("#category-selector");
+    //    let value = categorySelector.value;
 
     for (let i = 0; i < products.length; i++) {
         let product = products[i];
@@ -69,36 +69,36 @@ function fillTable(products) {
         let imageHolder = document.createElement("div");
         imageHolder.className = "block2-img wrap-pic-w of-hidden pos-relative";
         let image = document.createElement("img");
-        image.src = "images/item-02.jpg";
+        image.src = "data:image/png;base64, " + product.image;
         image.alt = "IMG-PRODUCT";
 
         let blockOverlay = document.createElement("div");
         blockOverlay.className = "block2-overlay trans-0-4";
         let addToCartHolder = document.createElement("div");
-         addToCartHolder.className = "block2-btn-addcart w-size1 trans-0-4";
-         let addToCartButton = document.createElement("button");
-         addToCartButton.className = "flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4";
-         addToCartButton.innerHTML = "Kosárba"
+        addToCartHolder.className = "block2-btn-addcart w-size1 trans-0-4";
+        let addToCartButton = document.createElement("button");
+        addToCartButton.className = "flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4";
+        addToCartButton.innerHTML = "Kosárba"
 
-         let textHolder = document.createElement("div");
-         textHolder.className = "block2-txt p-t-20";
-         let textHolderAnchor = document.createElement("a");
-         textHolderAnchor.href = "/product.html?address=" + product.address;
-         textHolderAnchor.className = "block2-name dis-block s-text3 p-b-5";
-         textHolderAnchor.innerHTML = product.producer + " - " + product.name;
-         let priceSpan = document.createElement("span");
-         priceSpan.className = "block2-price m-text6 p-r-5";
-         priceSpan.innerHTML = product.currentPrice + " Ft";
+        let textHolder = document.createElement("div");
+        textHolder.className = "block2-txt p-t-20";
+        let textHolderAnchor = document.createElement("a");
+        textHolderAnchor.href = "/product.html?address=" + product.address;
+        textHolderAnchor.className = "block2-name dis-block s-text3 p-b-5";
+        textHolderAnchor.innerHTML = product.producer + " - " + product.name;
+        let priceSpan = document.createElement("span");
+        priceSpan.className = "block2-price m-text6 p-r-5";
+        priceSpan.innerHTML = product.currentPrice + " Ft";
 
-         textHolder.appendChild(textHolderAnchor);
-         textHolder.appendChild(priceSpan);
-         addToCartHolder.appendChild(addToCartButton);
-         blockOverlay.appendChild(addToCartHolder);
-         imageHolder.appendChild(image);
-         imageHolder.appendChild(blockOverlay);
-         block.appendChild(imageHolder);
-         block.appendChild(textHolder);
-         productDiv.appendChild(block);
-         productContainer.appendChild(productDiv);
+        textHolder.appendChild(textHolderAnchor);
+        textHolder.appendChild(priceSpan);
+        addToCartHolder.appendChild(addToCartButton);
+        blockOverlay.appendChild(addToCartHolder);
+        imageHolder.appendChild(image);
+        imageHolder.appendChild(blockOverlay);
+        block.appendChild(imageHolder);
+        block.appendChild(textHolder);
+        productDiv.appendChild(block);
+        productContainer.appendChild(productDiv);
     }
 }
