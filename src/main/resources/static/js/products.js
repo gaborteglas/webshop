080acb2c4e917c11a5b453dbd2853a79beba4f12
Hajdú Mózes <hajdu.mozes@gmail.com>
2018-11-24 12:07:42 +0100

    addToBasket is available on products.html from now on

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -73,14 +73,15 @@ function fillTable(products) {
         let image = document.createElement("img");
         image.src = "data:image/png;base64, " + product.image;
         image.alt = "IMG-PRODUCT";
-        image.id = "productid-" + product.id;
 
         let blockOverlay = document.createElement("div");
         blockOverlay.className = "block2-overlay trans-0-4";
         let addToCartHolder = document.createElement("div");
         addToCartHolder.className = "block2-btn-addcart w-size1 trans-0-4";
         let addToCartButton = document.createElement("button");
-        addToCartButton.className = "flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4";
+        addToCartButton.className = "flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4 add-to-basket";
+        addToCartButton.id = "productid-" + product.id;
+        addToCartButton.onclick = activateAddToBasketButtons;
         addToCartButton.innerHTML = "Kosárba"
 
         let textHolder = document.createElement("div");
@@ -106,7 +107,99 @@ function fillTable(products) {
     }
 }
 
-function activateCartButton() {
-    let basketIcon = document.querySelector(".header-wrapicon2");
-    basketIcon.classList.remove("disabled");
+function activateAddToBasketButtons() {
+            let productId = this.id;
+            productId = productId.substring(productId.indexOf("-") + 1);
+            addToBasket(productId);
+}
+
+function addToBasket(productId) {
+    fetch("api/basket/" + productId + "/" + 1, {
+        method: "POST"
+    }).then(function (response) {
+        return response.json()
+    }).then(function (jsonData) {
+        giveFeedbackToUser(jsonData.message);
+        updateCart();
+    });
+    return false;
+}
+
+function updateCart() {
+    fetch("api/basket")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (products) {
+            fillCart(products);
+        });
+}
+
+function fillCart(products) {
+    let totalPrice = 0
+    let totalQuantity = 0;
+    let cart = document.querySelector(".header-cart-wrapitem");
+    cart.innerHTML = "";
+    let totalPriceField = document.querySelector(".header-cart-total");
+    let cartQuantity = document.querySelector(".header-icons-noti");
+    if (products.length === 0) {
+        let basketIcon = document.querySelector(".header-wrapicon2");
+        basketIcon.classList.add("disabled");
+    } else {
+        let basketIcon = document.querySelector(".header-wrapicon2");
+        basketIcon.classList.remove("disabled");
+        for (let k = 0; k < products.length; k++) {
+            let cartElement = document.createElement("li");
+            cartElement.className = "header-cart-item";
+
+            let imageHolderDiv = document.createElement("div");
+            imageHolderDiv.className = "header-cart-item-img";
+            imageHolderDiv.id = "deleteButtonId" + products[k].id;
+            imageHolderDiv.onclick = clickingOnResetProductButtons;
+
+            let imageOfProduct = document.createElement("img");
+            imageOfProduct.src = "data:image/png;base64, " + products[k].image;
+            imageOfProduct.alt = "IMG";
+
+            imageHolderDiv.appendChild(imageOfProduct);
+
+            let textHolderDiv = document.createElement("div");
+            textHolderDiv.className = "header-cart-item-txt";
+
+            let anchor = document.createElement("a");
+            anchor.href = "/product.html?address=" + products[k].address;
+            anchor.className = "header-cart-item-name";
+            anchor.innerHTML = products[k].name;
+
+            let price = document.createElement("span");
+            price.className = "header-cart-item-info";
+            price.id = "header-cart-price-" + products[k].id;
+            price.innerHTML = products[k].quantity + " x " + products[k].currentPrice.toLocaleString() + " Ft";
+
+            textHolderDiv.appendChild(anchor);
+            textHolderDiv.appendChild(price);
+
+            cartElement.appendChild(imageHolderDiv);
+            cartElement.appendChild(textHolderDiv);
+            cart.appendChild(cartElement);
+
+            totalPrice = totalPrice + (products[k].quantity * products[k].currentPrice);
+            totalQuantity += products[k].quantity;
+        }
+        totalPriceField.innerHTML = "Összesen: " + totalPrice.toLocaleString() + " Ft";
+    }
+    cartQuantity.innerHTML = totalQuantity;
+}
+
+function giveFeedbackToUser(message) {
+if (document.querySelector("#to-basket-feedback-message") !== null) {
+        document.querySelector("#to-basket-feedback-message").innerHTML = message;
+    } else {
+        let container = document.querySelector(".container");
+        let feedbackText = document.createElement("p");
+        feedbackText.className = "s-text8 p-t-10";
+        feedbackText.id = "to-basket-feedback-message";
+        feedbackText.innerHTML = message;
+        container.appendChild(feedbackText);
+    }
 }
