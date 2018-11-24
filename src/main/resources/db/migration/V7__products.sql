fc12a5d7e1a92893eee4ef796e621ac94b982e1b
gaborteglas <gaborteglas@gmail.com>
2018-11-24 14:45:02 +0100

    products.css and db changes

--- src/main/resources/db/migration/V7__products.sql
+++ src/main/resources/db/migration/V7__products.sql
@@ -5,10 +5,10 @@ ADD FOREIGN KEY (category_id) REFERENCES category(id);
 INSERT INTO products
 (name, address, producer, price, status, category_id)
 VALUES ('A szürke 50 árnyalata', 'a-szurke-50-arnyalata', 'E. L. James', 3999, 'ACTIVE', 5),
-('Junior fejlesztő falinaptár 2019', 'junior-fejleszto-falinaptar-2019', 'Peter Doe', 4699, 'ACTIVE', 1),
+('Junior fejlesztő falinaptár 2019', 'junior-fejleszto-falinaptar-2019', 'Kia Doe', 4699, 'ACTIVE', 1),
 ('Legendás állatok és megfigyelésük', 'legendas-allatok-es-megfigyelesuk','J. K. Rowling',  3999, 'ACTIVE', 2),
 ('Így neveld a sárkányodat', 'igy-neveld-a-sarkanyodat', 'Cressida Cowell', 2499, 'ACTIVE', 4),
-('A Gyűrűk ura: A gyűrű szövetsége', 'a-gyuruk-ura-a-gyuru-szovetsege', 'J.R.R. Doe', 2899, 'ACTIVE', 2),
+('A Gyűrűk ura: A gyűrű szövetsége', 'a-gyuruk-ura-a-gyuru-szovetsege', 'J.R.R. Tolkien', 2899, 'ACTIVE', 2),
 ('Cseresznyéskert', 'cseresznyeskert', 'Anton Csehov', 5599, 'ACTIVE', 4),
 ('Nemzeti Dal', 'nemzeti-dal', 'Petőfi Sándor', 3799, 'ACTIVE', 2),
 ('Az egri csillagok', 'az-egri-csillagok', 'Gárdonyi Géza', 4899, 'ACTIVE', 3),
