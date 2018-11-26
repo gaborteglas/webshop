5fba113bca483b7384c1f5b95ddeb7a13970adc3
Hajdú Mózes <hajdu.mozes@gmail.com>
2018-11-26 21:48:08 +0100

    added feedbacks in sql

---
+++ src/main/resources/db/migration/V14__addFeedbacks.sql
@@ -0,0 +1,18 @@
+INSERT INTO feedback
+(rating_text, rating_score, rating_date, product_id, user_id)
+VALUES
+('A Java a barátom: ebből a könyvből megtanultam, amire egy junior Java-fejlesztőnek szüksége lehet a mindennapi munkája során. Mostantól minden osztály nevét $-el kezdem, és igyekszem az egy metódusba tartozó sorokat külön oldalakra tördelni!', 5, '2018-11-20 11:30:51', 3, 5),
+('Kellemes fogalmazás, érthető magyarázat, ugyanakkor a haszna már felvet néhány kérdést.', 4, '2018-11-21 13:12:30', 3, 4),
+('Jó látni, hogy léteznek még olyan humoristák, akik igénylik, hogy gondolkozzanak és gondolkoztassanak!', 5, '2018-11-21 13:14:31', 11, 4),
+('Kerestem, de nem találtam benne végzetet sehol...', 3, '2018-11-21 13:16:31', 8, 4),
+('Lehetetlen ennek a könyvnek nem 5 csillagot adni, de az új fordítás több kárt okozott, mint hasznot.', 5, '2018-11-21 13:18:32', 13, 4),
+('Csak ajánlani tudom :)', 4, '2018-11-21 13:37:34', 15, 4),
+('A régebbi kiadások sokkal jobbak voltak... :(', 3, '2018-11-21 13:41:39', 9, 4),
+('Történnek balesetek az emberiség történetében. Ikarusz kipróbálta a szárnyait, Napóleon Oroszországot, majd E.L.James az írást.', 1, '2018-11-21 13:49:34', 1, 4),
+('Az ár mindent elárul...', 2, '2018-11-21 13:50:34', 6, 4),
+('Remekmű. De miért a Hugrabuggal kell reklámozni?', 5, '2018-11-21 13:51:38', 7, 4),
+('Jó gondolatok, trendszerű tálalás.', 4, '2018-11-21 13:55:39', 12, 4),
+('Ha valaminek van esélye megelőzni a Bibliát a ranglistákon, akkor az ez.', 5, '2018-11-21 13:56:30', 2, 4),
+('A gasztronómia jövője, de youtube-on még mindig egyszerűbb és költséghatékonyabb nézni.', 4, '2018-11-21 13:58:30', 4, 4),
+('Irodalmi alapmű, ami felett súlyosan eljárt már az idő. Ha valaki szerint a film unalmas, sosem volt még a kezében Tolkien könyv.', 3, '2018-11-21 14:56:30', 5, 4),
+('Van, hogy a füst nagyobb, mint a láng. A Trónok harca viszont egy betemetett pöcegödör, ami felé kamufüsstel csábítják oda a közeli katasztrófaturistákat.', 2, '2018-11-21 14:58:30', 14, 4);
